142975305dbf831f0d11f3033e32999f9364dcab
gravitygrey-dev <hayden_grey@hotmail.co.nz>
2023-08-27 18:15:52 +1200

    Header css and routes

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import Header from "./components/Header"
+import {Routes, Route } from "react-router-dom"
+import Tokens from "./components/Tokens"
+import Swap from "./components/Swap"
 
 function App() {
   return (
   <>
   <div className="App">
   <Header/>
+  <div className="mainWindow">
+    <Routes>
+    <Route path='/' element={<Swap/>} />
+    <Route path='/tokens' element={<Tokens/>} />
+    </Routes>
+    
+
+  </div>
   </div>
   
   </>
--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import Eth from "../eth.svg"
-import Logo from "../moralis-logo.svg"
+import React from "react";
+import Eth from "../eth.svg";
+import Logo from "../moralis-logo.svg";
 
-function Header() {
+function Header(connect) {
   return (
-    <div> 
+    <div>
       <header>
-        <div className='leftH'>
-          <img src={Logo} alt='logo' className='logo'/>
-          <div className='headerItem'>Swap</div>
-          <div className='headerItem'>Tokens</div>
-          hhhdgdg
+        <div className="leftH">
+          <img src={Logo} alt="logo" className="logo" />
+          <div className="headerItem">Swap</div>
+          <div className="headerItem">Tokens</div>
+          <div className="rightH">
+            <div className="headerItem">
+              <img src={Eth} alt="eth logo" className="eth" />
+              Ethereum
+            </div>
+            <div className="connectButton" onClick={connect}>
+Connect
+            </div>
+          </div>
         </div>
       </header>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default Header
+export default Header;
