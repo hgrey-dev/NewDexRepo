b4cdbb3bc5a869d660cff0ddd9242eb0b7573587
gravitygrey-dev <hayden_grey@hotmail.co.nz>
2023-08-27 18:53:30 +1200

    Update app css, swap css and AntDesign

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,26 +1,23 @@
 import "./App.css";
-import Header from "./components/Header"
-import {Routes, Route } from "react-router-dom"
-import Tokens from "./components/Tokens"
-import Swap from "./components/Swap"
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Tokens from "./components/Tokens";
+import Swap from "./components/Swap";
 
 function App() {
   return (
-  <>
-  <div className="App">
-  <Header/>
-  <div className="mainWindow">
-    <Routes>
-    <Route path='/' element={<Swap/>} />
-    <Route path='/tokens' element={<Tokens/>} />
-    </Routes>
-    
-
-  </div>
-  </div>
-  
-  </>
-    );
+    <>
+      <div className="App">
+        <Header />
+        <div className="mainWindow">
+          <Routes>
+            <Route path="/" element={<Swap />} />
+            <Route path="/tokens" element={<Tokens />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Input, Popover, Radio, Modal, message } from "antd";
+import {
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
 function Swap() {
-  return (
-    <div>Swap</div>
+
+  const setting = (
+    <>
+    <div>
+      Slippage Tolerance
+    </div>
+    </>
   )
+
+  return (
+    <div className="tradeBox">
+      <div className="tradeBoxHeader">
+        <h4>Swap</h4>
+        <Popover content={setting} title="Settings" trigger="click" placement="bottomRight">
+        <SettingOutlined className="cog"/>
+        </Popover>
+      </div>
+      <div className="inputs">
+
+      </div>
+    </div>
+  );
 }
 
-export default Swap
+export default Swap;
