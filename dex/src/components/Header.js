37beca1c6cfb0fb78906db8e6215c64c6ce66759
gravitygrey-dev <hayden_grey@hotmail.co.nz>
2023-08-27 16:51:34 +1200

    initial set up

---
+++ dex/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Eth from "../eth.svg"
+import Logo from "../moralis-logo.svg"
+
+function Header() {
+  return (
+    <div> 
+      <header>
+        <div className='leftH'>
+          <img src={Logo} alt='logo' className='logo'/>
+          <div className='headerItem'>Swap</div>
+          <div className='headerItem'>Tokens</div>
+          hhhdgdg
+        </div>
+      </header>
+      </div>
+  )
+}
+
+export default Header
